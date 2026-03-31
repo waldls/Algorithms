c8a4fce0e0accc802bafd2e062f754a559df27a3
YuminPark <p353700@gmail.com>
2026-04-01 00:01:58 +0900

    [Silver III] Title: 두 수의 합, Time: 84 ms, Memory: 42660 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = list(map(int, input().split()))
+x = int(input())
+
+arr.sort()
+
+left = 0
+right = n - 1
+count = 0
+
+while left < right:
+    s = arr[left] + arr[right]
+
+    if s == x:
+        count += 1
+        left += 1
+        right -= 1
+    elif s < x:
+        left += 1
+    else:
+        right -= 1
+
+print(count)
