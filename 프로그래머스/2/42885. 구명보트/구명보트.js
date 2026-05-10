c72ceeb80b6d7c8539236c58c337944fae990c23
YuminPark <p353700@gmail.com>
2026-05-11 04:56:52 +0900

    [level 2] Title: 구명보트, Time: 23.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,15 @@
+function solution(people, limit) {
+    people.sort((a, b) => a - b);
+    let left = 0, right = people.length - 1;
+    let boats = 0;
+    
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            left++;
+        }
+        right--;
+        boats++;
+    }
+    
+    return boats;
+}
