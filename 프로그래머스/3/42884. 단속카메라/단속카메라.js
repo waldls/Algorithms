6e7000b40ac369408e742d93c863bdd79753ad4a
YuminPark <p353700@gmail.com>
2026-06-03 13:53:19 +0900

    [level 3] Title: 단속카메라, Time: 1.88 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -0,0 +1,15 @@
+function solution(routes) {
+    routes.sort((a, b) => a[1] - b[1]);
+    
+    let count = 1;
+    let camera = routes[0][1];
+    
+    for (let i = 1; i < routes.length; i++) {
+        if (camera < routes[i][0]) {
+            camera = routes[i][1];
+            count++;
+        }
+    }
+    
+    return count;
+}
